cbcbcf5e437934e925262ef9ad7082693af0d64b
tomeckert1 <tomeckert1@gmail.com>
2025-07-23 23:53:12 -0700

    Update backend.py

--- backend.py
+++ backend.py
@@ -2,7 +2,7 @@ import requests
 
 def get_response(question: str):
     response = requests.post(
-        "https://168b5f18-1a60-4338-b72f-69d17778facf-00-1melv1wth7iy8.worf.replit.dev/query",  # 👈 your Replit backend URL
+        "https://168b5f18-1a60-4338-b72f-69d17778facf-00-1melv1wth7iy8.worf.replit.dev/ask",  # 👈 your Replit backend URL
         json={"query": question}
     )
     response_json = response.json()
